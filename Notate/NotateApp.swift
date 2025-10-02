f1e5cd63047b22330864e967b473ed018438fb87
Victor Zhang <cyzhangv@umich.edu>
2025-10-02 16:06:06 -0400

    Initial Commit

---
+++ Notate/NotateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotateApp.swift
+//  Notate
+//
+//  Created by VictorZzz on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct NotateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
